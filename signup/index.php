3cacc39fa09a22476fb991a48f49b2cc8ce7694c
Ahmad <ahmadalkali.dev@gmail.com>
2024-11-21 17:12:05 +0100

    Implemented user documents upload on signup

--- signup/index.php
+++ signup/index.php
@@ -5,9 +5,16 @@
 <link rel="stylesheet" href="./signup.css">
 
 <h1>Sign Up</h1>
-<form action="../includes/signup.inc.php" method="post">
+<form action="../includes/signup.inc.php" method="post" enctype="multipart/form-data">
   <?php signup_inputs(); ?>
 </form>
+
+<script src="./script.js"></script>
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    setupRoleToggle();
+  });
+</script>
 
 <?php check_signup_errors() ?>
 
