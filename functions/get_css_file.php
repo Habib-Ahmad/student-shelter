56e91d263df89a980d4e9d5c7757827c6e8db539
vincenchi <osundeamenze@gmail.com>
2024-12-16 23:51:18 +0100

    Created terms and conditions, faq and legal notice pages

--- functions/get_css_file.php
+++ functions/get_css_file.php
@@ -23,6 +23,15 @@ function get_css_file()
     case 'contact':
       $css_file = "{$BASEURL}contact.css";
       break;
+    case 'faq':
+      $css_file = "{$BASEURL}faq.css";
+      break;
+    case 'terms_conditions':
+      $css_file = "{$BASEURL}terms_conditions.css";
+      break;
+    case 'legal':
+      $css_file = "{$BASEURL}legal.css";
+      break;
 
     default:
       break;
