af9e5f6820112eaa02a2d497a67996007482013f
Ahmad <ahmadalkali.dev@gmail.com>
2025-01-20 19:56:01 +0100

    Fixed favorites page

--- includes/models/favorites_model.php
+++ includes/models/favorites_model.php
@@ -4,10 +4,33 @@ declare(strict_types=1);
 
 function get_user_favorites(object $pdo, int $userId)
 {
-  $sql = "SELECT favorites.id, monthlyPrice, name, unit.type, streetAddress, city, postalCode, unit.id as unitId FROM favorites LEFT JOIN unit ON favorites.unitId = unit.id LEFT JOIN property ON unit.propertyId = property.id WHERE favorites.userId = :userId";
+  $sql = "
+        SELECT 
+            favorites.id, 
+            monthlyPrice, 
+            name, 
+            unit.type, 
+            streetAddress, 
+            city, 
+            postalCode, 
+            unit.id AS unitId,
+            GROUP_CONCAT(unit_images.image) AS images
+        FROM 
+            favorites
+        LEFT JOIN 
+            unit ON favorites.unitId = unit.id
+        LEFT JOIN 
+            property ON unit.propertyId = property.id
+        LEFT JOIN 
+            unit_images ON unit.id = unit_images.unitId
+        WHERE 
+            favorites.userId = :userId
+        GROUP BY 
+            favorites.id, monthlyPrice, name, unit.type, streetAddress, city, postalCode, unit.id
+    ";
   $stmt = $pdo->prepare($sql);
   $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
   $stmt->execute();
 
-  return $stmt->fetchAll();
+  return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
